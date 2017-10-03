2ecb129b6f98792955eb96d3c3c2261743fc18ca
Danish <Danish@Danishs-MacBook-Pro.local>
2017-10-02 21:22:20 -0400

    Testing for Addthis

--- js/scroll_to.js
+++ js/scroll_to.js
@@ -1,15 +1,25 @@
 $(function() {
 
+
+
 	$('a[href^="#"]').on('click', function(event) {
     	var target = $(this.getAttribute('href'));
+      var $window = $(window);
 
     	if( target.length ) {
       	  event.preventDefault();
       	  $navList.slideToggle();
 		  $butLine.toggleClass('open');
+
+      if ($window.width() >= 1000) {
       	  $('html, body').stop().animate({
-       	     scrollTop: target.offset().top - 120
+       	     scrollTop: target.offset().top
         }, 600);
+      } else {
+        $('html, body').stop().animate({
+             scrollTop: target.offset().top - 120
+        }, 600);
+      }
     }
 });
 
