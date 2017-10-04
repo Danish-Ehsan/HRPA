7c8a0494da0e2847c99e77dbfe6759c2da11925d
Danish <Danish@Danishs-MacBook-Pro.local>
2017-10-04 07:03:02 -0400

    Animation Tweaks

--- js/animation_rotate.js
+++ js/animation_rotate.js
@@ -5,6 +5,7 @@ $(function() {
 	var $intelRevo = $('#intelRevoImg')
 
 	var deg = 0;
+	//var spinReset = false;
 
 	//var spinCheck = false;
 	//var funcCheck = true;
@@ -12,9 +13,13 @@ $(function() {
 	function spin(e) {
 		setInterval(function() {
 			if ( (($window.scrollTop() + $window.height()) > e.offset().top) && $window.scrollTop() < (e.offset().top + e.height())) {
+				//spinReset = true;
 				deg = deg + 0.5;
 				console.log(deg);
 				e.css('-webkit-transform', 'rotate(' + deg + 'deg)');
+			} else if ( $window.scrollTop() > ($intelRevo.offset().top + e.height()) && (($window.scrollTop() + $window.height()) < $archWhl.offset().top) ) {
+				//spinReset = true;
+				deg = 0;
 			}
 		}, 100);
 	}
